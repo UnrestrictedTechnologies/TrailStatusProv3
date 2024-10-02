270021c72ad88542fca95634e43ec03b56cc9056
Eric Forbes <66398324+UnrestrictedTechnologies@users.noreply.github.com>
2024-10-02 13:17:48 -0400

    Initial Commit

---
+++ TrailStatusProv3/TrailStatusProv3App.swift
@@ -0,0 +1,17 @@
+//
+//  TrailStatusProv3App.swift
+//  TrailStatusProv3
+//
+//  Created by Eric Forbes on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TrailStatusProv3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
